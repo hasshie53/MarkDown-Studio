3dc6d6c591783d7f75433daa483bcd972b631de9
hasshie53 <yuta9853@gmail.com>
2026-04-13 18:48:57 +0900

    Tauriの導入設定およびビルド生成物を除外するための.gitignore更新

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,21 @@
-import { defineConfig } from 'vite'; export default defineConfig({ base: '/MarkDown-Studio/' });
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Tauri expect a fixed port, fail if that port is not available
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  // to make use of `import.meta.env.TAURI_PLATFORM` and other env variables
+  // https://tauri.app/v1/api/config#buildconfig.beforedevcommand
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    // Tauri supports esbuild's target, and since we're on Windows primarily, we target chrome105
+    target: process.env.TAURI_ENV_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
+});
